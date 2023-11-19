dac371cfc6f78b8987a389ed21192150928c0160
João Netto <joaonetto@meteora.academy>
2023-11-19 16:12:42 -0300

    feat: adds Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: npm run start:staging
+web: npm run start:prod
--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,18 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import cors from "cors";
 
-import prisma from "./utils/prismaClient"
 import plansRouter from "./routes/plans";
 import radiusUsersRouter from "./routes/radiusUsers";
 import teamsRouter from "./routes/teams";
 import authRouter from "./routes/auth";
-import { seedDatabase } from "./prisma/seeds/boilerplate"
 import { swaggerOptions } from "./utils/globalVariables";
 
 const app = express();
 const swaggerJsdoc = require("swagger-jsdoc");
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -24,14 +22,6 @@ app.use("/radiususers", radiusUsersRouter);
 app.use("/teams", teamsRouter);
 app.use("/auth", authRouter);
 
-seedDatabase()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-    
-    app.listen(3000, "0.0.0.0", () => {
-      console.log(`Server is running on http://0.0.0.0:3000 - ${process.env.NODE_ENV} Evironment`);
-    });
-  });
+app.listen(3000, "0.0.0.0", () => {
+  console.log(`Server is running on http://0.0.0.0:3000 - ${process.env.NODE_ENV} Evironment`);
+});
